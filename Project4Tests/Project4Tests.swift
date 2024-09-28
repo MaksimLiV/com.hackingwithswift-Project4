90e2054bea6169034cc8a0e0ca73cc7696ae1291
Maksim Li <maximlee.ml@gmail.com>
2024-09-28 22:05:48 +0300

    Initial Commit

---
+++ Project4Tests/Project4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Project4Tests.swift
+//  Project4Tests
+//
+//  Created by Maksim Li on 26/09/2024.
+//
+
+import Testing
+@testable import Project4
+
+struct Project4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
