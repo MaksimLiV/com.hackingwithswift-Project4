fb4ce6b933b7f00e835270239bc940080a0e5023
Maksim Li <maximlee.ml@gmail.com>
2024-10-04 00:16:02 +0300

    4th project

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -1,8 +1,10 @@
 import UIKit
-import WebKit
+@preconcurrency import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
+    var progressView: UIProgressView!
+    var websites = ["hackingwithswift.com", "facebook.com", "apple.com"]
     
     override func loadView() {
         webView = WKWebView()
@@ -14,18 +16,43 @@ class ViewController: UIViewController, WKNavigationDelegate {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let backButton = UIBarButtonItem(title: "Back", style: .plain, target: webView, action: #selector(webView.goBack))
+        
+        let forwardButton = UIBarButtonItem(title: "Forward", style: .plain, target: webView, action: #selector(webView.goForward))
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        
+        
+        toolbarItems = [backButton, forwardButton, progressButton, spacer, refresh]
+        navigationController?.setToolbarHidden(false, animated: true)
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
       
-        let url = URL(string: "https://www.hackingwithswift.com")!
+        let url = URL(string: "https://" + websites[0])!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
     
     @objc func openTapped() {
         let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+    
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
+        
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        
+        if let popover = ac.popoverPresentationController {
+                   popover.barButtonItem = navigationItem.rightBarButtonItem
+               }
+        
         present(ac, animated: true)
     }
     
@@ -39,8 +66,35 @@ class ViewController: UIViewController, WKNavigationDelegate {
         title = webView.title
     }
     
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+            if keyPath == "estimatedProgress" {
+                progressView.progress = Float(webView.estimatedProgress)
+            }
+        }
+        
+        deinit {
+            
+            webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress))
+        }
     
-    
-    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        
+        let ac = UIAlertController(title: "Blocked", message: "This website is blocked.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        
+        decisionHandler(.cancel)
+        
+    }
     
 }
